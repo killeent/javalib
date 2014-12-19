ef07b4846d37f7bb7a0bac0c286161e7d988f999
Trevor Killeen <killeent@cs.washington.edu>
2014-12-19 15:32:18 -0800

    add array search for first occurrrence and test

--- src/test/java/com/killeent/ArraySearchTest.java
+++ src/test/java/com/killeent/ArraySearchTest.java
@@ -52,4 +52,52 @@ public class ArraySearchTest {
         Assert.assertEquals(2, ArraySearch.binarySearch(input, integerComparator, 3));
     }
 
+    /**
+     * Tests for {@link com.killeent.Array.ArraySearch#firstOccurrence}.
+     */
+
+    /**
+     * Tests searching an empty array.
+     */
+    @Test
+    public void testFirstOccurrenceEmptyArray() {
+        Integer[] input = new Integer[0];
+        Assert.assertEquals(-1, ArraySearch.firstOccurrence(input, integerComparator, 0));
+    }
+
+    /**
+     * Tests searching a single element array.
+     */
+    @Test
+    public void testFirstOccurrenceSingleElementArray() {
+        Integer[] input = new Integer[]{1};
+        Assert.assertEquals(-1, ArraySearch.firstOccurrence(input, integerComparator, 0));
+        Assert.assertEquals(0, ArraySearch.firstOccurrence(input, integerComparator, 1));
+    }
+
+    /**
+     * Test searching a multi-element array without duplicates.
+     */
+    @Test
+    public void testFirstOccurrenceMultiElementArrayNoDupes() {
+        Integer[] input = new Integer[]{1, 2, 3};
+        Assert.assertEquals(-1, ArraySearch.firstOccurrence(input, integerComparator, 0));
+        Assert.assertEquals(0, ArraySearch.firstOccurrence(input, integerComparator, 1));
+        Assert.assertEquals(1, ArraySearch.firstOccurrence(input, integerComparator, 2));
+        Assert.assertEquals(2, ArraySearch.firstOccurrence(input, integerComparator, 3));
+    }
+
+    /**
+     * Test searching a multi-element array without duplicates.
+     */
+    @Test
+    public void testFirstOccurrenceMultiElementArrayWithDupes() {
+        Integer[] input = new Integer[]{1, 1, 2, 2, 3, 3, 4};
+        Assert.assertEquals(-1, ArraySearch.firstOccurrence(input, integerComparator, 0));
+        Assert.assertEquals(0, ArraySearch.firstOccurrence(input, integerComparator, 1));
+        Assert.assertEquals(2, ArraySearch.firstOccurrence(input, integerComparator, 2));
+        Assert.assertEquals(4, ArraySearch.firstOccurrence(input, integerComparator, 3));
+        Assert.assertEquals(6, ArraySearch.firstOccurrence(input, integerComparator, 4));
+    }
+
 }
