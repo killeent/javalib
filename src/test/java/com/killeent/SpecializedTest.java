6203c0b895e85b34831cdd25d9311e0c67820655
Trevor Killeen <killeent@cs.washington.edu>
2015-01-17 16:53:57 -0800

    add stub test class for Specialized

---
+++ src/test/java/com/killeent/SpecializedTest.java
@@ -0,0 +1,12 @@
+package com.killeent;
+
+/**
+ * Tests for {@link com.killeent.Specialized.Specialized}.
+ *
+ * @author Trevor Killeen (2015)
+ */
+public class SpecializedTest {
+
+    // todo: add tests for minimal division
+
+}
